41fcc1f776833e0783e9dd2b628486129969ba17
Jannik Bühring <jannik.buehring1@web.de>
2025-10-02 08:43:23 +0200

    make linter happy

--- models/draw_data.py
+++ models/draw_data.py
@@ -1,8 +1,10 @@
+"""Data structures for draw data input."""
 from models.player import players_by_start_number
 
 seeding_by_start_numbers = {}
 
 class DrawDataRow:
+    """Data structure for a row in the draw data input."""
     def __init__(self, competition, competition_class, seeding: int, amount_of_groups: int, group_no: int, group_pos: int, main_round: bool, consolation_round: bool, start_number_a: int, start_number_b: int):
         self.competition = competition
         self.competition_class = competition_class
@@ -15,7 +17,7 @@ class DrawDataRow:
         self.start_number_a = int(start_number_a)
         self.start_number_b = int(start_number_b) if start_number_b != '' else None
         
-        if self.seeding != None:
+        if self.seeding is not None:
             key = str(start_number_a)
             if self.start_number_b is not None:
                 key += "/" + str(start_number_b)
@@ -23,3 +25,4 @@ class DrawDataRow:
 
     def __repr__(self):
         return f"{self.competition} {self.competition_class} (Seeding: {self.seeding}, Player A: {self.start_number_a}, {players_by_start_number[self.start_number_a].country}. Player B: {self.start_number_b}, {players_by_start_number[self.start_number_b].country if self.start_number_b is not None else ''})"
+    
